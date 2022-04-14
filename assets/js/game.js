caab602343d0d46fa3a7f352e736461a7ee39c4f
Jim Gryck <halo2382001@yahoo.com>
2022-04-14 10:52:31 -0400

    require a name in robot name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,3 @@
-
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
@@ -157,11 +156,21 @@ switch (shopOptionPrompt) {
     shop();
     break;
 }
-
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
